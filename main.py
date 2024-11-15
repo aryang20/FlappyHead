5a80e093af983ca6dcb3deef6f94a4af7f41bb37
Aryan <aryan@gupta-inc.com>
2024-11-15 15:23:51 -0800

    oldtownroad

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import cv2 as cv, pygame as pg, mediapipe as mp, sys, time, random
+import numpy
 
-    
+pg.init()
+
+screenWidth = 800
+screenHeight = 800
+
+screen = pg.display.set_mode([screenWidth, screenHeight])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
