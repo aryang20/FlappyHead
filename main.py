c03423c067ff753e3e9af7d7d71b7c9d50cad08e
Aryan <aryan@gupta-inc.com>
2024-11-15 16:10:22 -0800

    x

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-import cv2 as cv, pygame as pg, mediapipe as mp, sys, time, random
-import numpy
-
-pg.init()
-
+import cv2 
+import mediapipe as mp
+import sys, time, random
+import pygame
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+pygame.init()
+cap = cv2.VideoCapture(0)
 screenWidth = 800
 screenHeight = 800
+screen = pygame.display.set_mode([screenWidth, screenHeight])
+running = True
+while  running:
+  for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+  ret, frame = cap.read(0)
+  if not ret:
+    break
+  
+  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+  fliped_jawn = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+  frame_surface = pygame.surfarray.make_surface(fliped_jawn)
+  screen.blit(frame_surface, (0, 0))
+  pygame.display.flip()
+  
+  
+  
 
-screen = pg.display.set_mode([screenWidth, screenHeight])
+
 
 
 
