ac1fb18317aaf89f8e3a7108c727835108c4cf5f
Aryan <aryan@gupta-inc.com>
2024-11-15 16:51:13 -0800

    adding face detection

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
-import cv2 
+import cv2
 import mediapipe as mp
 import sys, time, random
 import pygame
+
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
 pygame.init()
 cap = cv2.VideoCapture(0)
 screenWidth = 800
 screenHeight = 800
 screen = pygame.display.set_mode([screenWidth, screenHeight])
 running = True
-while  running:
-  for event in pygame.event.get():
+
+
+pipePath = "/images/pipe_sprite_single.png"
+flapPath = "/images/flap.png"
+
+
+while running:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-  ret, frame = cap.read(0)
-  if not ret:
-    break
-  
-  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-  fliped_jawn = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-  frame_surface = pygame.surfarray.make_surface(fliped_jawn)
-  screen.blit(frame_surface, (0, 0))
-  pygame.display.flip()
-  
-  
-  
+
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = face_detection.process(frame_rgb)
+
+    if results.detections:
+        for detection in results.detections:
+            mp_drawing.draw_detection(frame, detection)
+
+    flipped_frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    flipped_frame_rgb = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2RGB)
+    frame_surface = pygame.surfarray.make_surface(flipped_frame_rgb)
+    screen.blit(frame_surface, (0, 0))
+    pygame.display.flip()
+
 
 
 
